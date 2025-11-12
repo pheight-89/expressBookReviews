fb6f04855000770b51c15d09f85fdbbdae5eabec
pheight-89 <209817625+pheight-89@users.noreply.github.com>
2025-11-11 19:40:52 -0500

    Added logic for returning all books in the shop, getting books by ISBN, and by authors.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,34 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if(book){
+    return res.status(200).json(book);
+  } else {
+    return res.status(400).json({ message: `Book with ISBN ${isbn} not found.`});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  const booksToFilter = Object.values(books);
+  const filteredBooks = booksToFilter.filter((book) => book.author === author);
+
+  if (filteredBooks.length > 0){
+    return res.status(200).json({ booksbyauthor: filteredBooks });
+  } else {
+    return res.status(400).json({ message: `No books found by ${author}.`});
+  }
 });
 
 // Get all books based on title
