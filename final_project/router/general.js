2affbc2a3b4c4d3e2ba90e02e4b249f2cc6b0c2a
pheight-89 <209817625+pheight-89@users.noreply.github.com>
2025-11-11 20:36:35 -0500

    Added logic for finding books by title and book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,20 +39,36 @@ public_users.get('/author/:author',function (req, res) {
   if (filteredBooks.length > 0){
     return res.status(200).json({ booksbyauthor: filteredBooks });
   } else {
-    return res.status(400).json({ message: `No books found by ${author}.`});
+    return res.status(404).json({ message: `No books found by ${author}.`});
   }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  const booksToFilter = Object.values(books);
+  const filteredBooks = booksToFilter.filter((book) => book.title === title);
+
+  if (filteredBooks.length > 0){
+    return res.status(200).json({ booksbytitle: filteredBooks });
+  } else {
+    return res.status(404).json({ message: `No books found by ${title}.`});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+let isbn = req.params.isbn;
+const book = books[isbn];
+
+if (book){
+    const reviews = book.reviews;
+    return res.status(200).json({reviews: reviews});
+} else {
+    return res.status(404).json({ message: `Book with ${isbn} not found.`});
+}
 });
 
 module.exports.general = public_users;
